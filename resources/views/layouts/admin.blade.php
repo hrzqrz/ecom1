5f0086125dfe4c87f93248d761f93ab48213b88b
hrzqrz <hrzqrz@gmail.com>
2020-06-24 17:27:58 +0430

    Implementing admin pannel part b

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>@yield('title')</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="{{asset('backend/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('backend/css/vendor.bundle.base.css')}}">
+  <link rel="stylesheet" href="{{asset('backend/css/vendor.bundle.addons.css')}}">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{asset('backend/css/style.css')}}">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="{{asset('backend/images/logo_2H_tech.png')}}" />
+  @yield('styles')
+</head>
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    @include('layouts.includes.admin._top')
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+    @include('layouts.includes.admin._sidebar')
+      <!-- partial -->
+      @yield('content')
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="{{asset('backend/js/vendor.bundle.base.js')}}"></script>
+  <script src="{{asset('backend/js/vendor.bundle.addons.js')}}"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="{{asset('backend/js/off-canvas.js')}}"></script>
+  <script src="{{asset('backend/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('backend/js/template.js')}}"></script>
+  <script src="{{asset('backend/js/settings.js')}}"></script>
+  <script src="{{asset('backend/js/todolist.js')}}"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="{{asset('backend/js/dashboard.js')}}"></script>
+  <!-- End custom js for this page-->
+  @yield('scripts')
+</body>
+
+</html>
+
---
+++ resources/views/layouts/includes/admin/_sidebar.blade.php
@@ -0,0 +1,40 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="ti-home menu-icon"></i>
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+          <i class="ti-clipboard menu-icon"></i>
+          <span class="menu-title">Form elements</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="form-elements">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"><a class="nav-link" href="basic_elements.html">Basic Elements</a></li>
+            <li class="nav-item"><a class="nav-link" href="advanced_elements.html">Advanced Elements</a></li>
+            <li class="nav-item"><a class="nav-link" href="validation.html">Validation</a></li>
+            <li class="nav-item"><a class="nav-link" href="wizard.html">Wizard</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
+          <i class="ti-layout menu-icon"></i>
+          <span class="menu-title">Tables</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="tables">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="basic-table.html">Basic table</a></li>
+            <li class="nav-item"> <a class="nav-link" href="data-table.html">Data table</a></li>
+            <li class="nav-item"> <a class="nav-link" href="js-grid.html">Js-grid</a></li>
+            <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Sortable table</a></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </nav>
